83241dd071cf9efe9e1f512306e49eb4fd9c52b9
Anastasia Dukhnich <vt242_dag@student.ztu.edu.ua>
2026-05-15 09:40:15 +0300

    refactor: remove internal validation from MealEntry to rely on external validators

--- src/models/MealEntry.js
+++ src/models/MealEntry.js
@@ -1,58 +1,45 @@
 export default class MealEntry {
     constructor(id, product, weightInGrams, date = new Date()) {
-        this.validate(product, weightInGrams);
+        if (!product) throw new Error("Продукт обов'язковий для запису прийому їжі.");
+        if (weightInGrams <= 0) throw new Error("Вага порції повинна бути більшою за нуль.");
 
-        this.id = id;
-        this.product = product;
+        this.id            = id;
+        this.product       = product;
         this.weightInGrams = weightInGrams;
-        this.date = date;
-
-        this.totalCalories = this.product.calculateCaloriesForWeight(this.weightInGrams);
-        this.totalMacros = this.product.calculateMacrosForWeight(this.weightInGrams);
-    }
-
-    validate(product, weight) {
-        if (!product) {
-            throw new Error("Продукт обов'язковий для запису прийому їжі.");
-        }
-        if (weight <= 0) {
-            throw new Error("Вага порції повинна бути більшою за нуль.");
-        }
+        this.date          = date;
+        this.totalCalories = product.calculateCaloriesForWeight(weightInGrams);
+        this.totalMacros   = product.calculateMacrosForWeight(weightInGrams);
     }
 
     getSummary() {
         return {
-            name: this.product.name,
-            weight: this.weightInGrams,
+            name:     this.product.name,
+            weight:   this.weightInGrams,
             calories: this.totalCalories.toFixed(1),
             proteins: this.totalMacros.proteins.toFixed(1),
-            fats: this.totalMacros.fats.toFixed(1),
-            carbs: this.totalMacros.carbs.toFixed(1),
-            time: this.date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+            fats:     this.totalMacros.fats.toFixed(1),
+            carbs:    this.totalMacros.carbs.toFixed(1),
+            time:     this.date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
         };
     }
 
     getCalorieBreakdown() {
-        const total = this.totalCalories;
-        const fromProteins = this.totalMacros.proteins * 4;
-        const fromFats     = this.totalMacros.fats * 9;
-        const fromCarbs    = this.totalMacros.carbs * 4;
         return {
-            fromProteins: parseFloat(fromProteins.toFixed(1)),
-            fromFats:     parseFloat(fromFats.toFixed(1)),
-            fromCarbs:    parseFloat(fromCarbs.toFixed(1)),
-            total:        parseFloat(total.toFixed(1))
+            fromProteins: parseFloat((this.totalMacros.proteins * 4).toFixed(1)),
+            fromFats:     parseFloat((this.totalMacros.fats * 9).toFixed(1)),
+            fromCarbs:    parseFloat((this.totalMacros.carbs * 4).toFixed(1)),
+            total:        parseFloat(this.totalCalories.toFixed(1))
         };
     }
 
-    isHighCalorie(threshold = 500) {
+    isHighCalorie(threshold = 400) {
         return this.totalCalories > threshold;
     }
 
     toJSON() {
         return {
             id:            this.id,
-            product:       this.product.toJSON ? this.product.toJSON() : this.product,
+            product:       this.product.toJSON(),
             weightInGrams: this.weightInGrams,
             date:          this.date,
             totalCalories: this.totalCalories,
